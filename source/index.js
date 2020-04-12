56a817c43ac326719f54d35736b870291ece2e43
Ben Zabloski <ben.zabloski@gmail.com>
2020-04-12 09:57:20 -0400

    Hooked up tv db service.

--- source/index.js
+++ source/index.js
@@ -1,15 +1,42 @@
+require("dotenv").config();
+
 const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
+const TVDB = require("node-tvdb");
+
+const tvdb = new TVDB(process.env.THE_TV_DB_API_KEY);
 
 const typeDefs = gql`
+  type Series {
+    id: String!
+    seriesName: String!
+  }
+
   type Query {
-    hello: String
+    getSeriesByID(id: String!): Series
+    getSeriesByName(name: String): [Series]
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "world",
+    getSeriesByID: async (_, { id }) => {
+      const result = await tvdb
+        .getSeriesById(id)
+        .then((response) => response)
+        .catch((error) => console.log(`getSeriesByID error: ${error}`));
+
+      return result;
+    },
+
+    getSeriesByName: async (_, { name }) => {
+      const result = await tvdb
+        .getSeriesByName(name)
+        .then((response) => response)
+        .catch((error) => console.log(`getSeriesByName error: ${error}`));
+
+      return result;
+    },
   },
 };
 
